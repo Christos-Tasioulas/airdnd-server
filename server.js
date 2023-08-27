a43182a9679491b35a6191fa84fc17f9be07b08b
User <christasioulas2@gmail.com>
2023-08-27 18:24:49 +0300

    Added photo handling
    Co-authored-by: VaggelisPapako <VaggelisPapako@users.noreply.github.com>

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const express = require('express')
 const cors = require("cors");
 const dotenv = require('dotenv');
 const https = require("https")
-const http = require("http")
 const path = require("path")
 const fs = require("fs")
+const fileUpload = require('express-fileupload');
 
 const app = express()
 
@@ -46,6 +46,44 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Use express-fileupload middleware
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        safeFileNames: true,
+        preserveExtension: true,
+        tempFileDir: `${__dirname}/public/`
+    })
+);
+
+app.post('/upload', function(req, res) {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({ message: 'No files were uploaded.' });
+    }
+
+    // The name of the input field (i.e., "image") should match the name on the client side
+    const uploadedImage = req.files.image;
+
+    // Generate a unique filename using MD5 hash and original filename
+    const originalFilename = uploadedImage.name;
+    const md5Hash = require('crypto').createHash('md5').update(originalFilename).digest('hex');
+    const uniqueFilename = `${md5Hash}_${originalFilename}`;
+
+    // Move the uploaded file to the desired location
+    uploadedImage.mv(`${__dirname}/public/${uniqueFilename}`, function(err) {
+        if (err) {
+            return res.status(500).json({ error: 'Error uploading file.' });
+        }
+
+        // Return a response indicating success
+        return res.status(200).json({ status: 'uploaded', filename: uniqueFilename });
+    });
+});
+
 
 // User Routes
 const userRouter = require('./routes/userRoutes.js')
